108a0046a831502baec5a49bad9e99d484a11475
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-04-05 09:39:51 -0400

    Create gameNight - DistributionsPogs.lua

---
+++ Contents/mods/gamenight_pogs/media/lua/server/gameNight - DistributionsPogs.lua
@@ -0,0 +1,7 @@
+require "Items/SuburbsDistributions"
+
+local gameNightDistro = require "gameNight - Distributions"
+
+--gameNightDistro.proceduralDistGameNight.itemsToAdd[""] = {chanceFactor = 0.5}
+--gameNightDistro.proceduralDistGameNight.itemsToAdd[""] = {chanceFactor = 0.5}
+--gameNightDistro.proceduralDistGameNight.itemsToAdd[""] = {chanceFactor = 0.5}
