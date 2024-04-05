f27e6b1558e53ae5456301245be689cc05ced121
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-04-05 09:39:49 -0400

    Create gameNight - Pogs recipes.lua

---
+++ Contents/mods/gamenight_pogs/media/lua/server/gameNight - Pogs recipes.lua
@@ -0,0 +1,7 @@
+require "recipecode"
+require "gameNight - recipes"
+
+---@param items ArrayList
+---@param result InventoryItem|InventoryContainer
+---@param player IsoPlayer
+--function Recipe.GameNight.OpenPogBooster(items, result, player) result:getModData()["gameNight_specialOnCardApplyBoosters"] = 4 end
